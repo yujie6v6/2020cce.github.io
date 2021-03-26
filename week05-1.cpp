42da62fa8a2135de985c72accccc76013cdc9bdb
yujie6v6 <79676845+yujie6v6@users.noreply.github.com>
2021-03-26 10:03:41 +0800

    Add files via upload

---
+++ week05-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+        char line[10]="decline";
+        char line2[10]={'p','r','o','p','e','r','\0'};
+
+        printf("%s\n",line);
+        printf("%s\n",line2);
+
+}
+
